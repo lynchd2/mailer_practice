b5b3017533532dcba91fa29a6e5b6f3864f1afdf
jsteenb2 <jonathansteenbergen@gmail.com>
2016-08-22 09:15:02 -0700

    updated production config for cedar stack

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,21 @@ Rails.application.configure do
     s3_region: Rails.application.secrets.S3_REGION
     }
   }
+
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.delivery_method ||= :smtp
+
+  # Rails also needs to know where your app is
+  #   located to properly configure sending of emails
+  config.action_mailer.default_url_options = {
+    :host => 'www.putyourdomainhere.com',
+  }
 end
